ef4144f85da73f6b63a53e0097131121cb70ee83
efriedla <efriedla20852@gmail.com>
2018-02-05 15:10:00 -0800

    stopped using local object and now shows whats in the database

--- app.js
+++ app.js
@@ -21,39 +21,47 @@ task: String
 
 var Task = mongoose.model('Task', taskSchema);
 
-Task.create({
-    task: "eat"
+// Task.create({
+//     task: "sleep"
       
-}, function(err, task){
-    if(err){
-        console.log(err);
-    }else{
-        console.log(task);
-    }
-});
-Task.find({}, function(err, tasks){
-    if(err){
-        console.log("nooo");
-        console.log(err)
-    }else{
-        console.log("here are my cats");
-        console.log(tasks);
-    }
-});
+// }, function(err, task){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(task);
+//     }
+// });
+
+// Task.find({}, function(err, tasks){
+//     if(err){
+//         console.log("nooo");
+//         console.log(err)
+//     }else{
+//         console.log("here are my tasks");
+//         console.log(tasks);
+//     }
+// });
 
 
 app.get("/", function(req, res){
     res.render("home");
 });
 
-var todos = [
-    {task: "eat"},
-    {task: "sleep"},
-    {task: "blink"}
-];
+// var todos = [
+//     {task: "eat"},
+//     {task: "sleep"},
+//     {task: "blink"}
+// ];
 app.get("/todo", function(req, res){
-    res.render("todo", {todos: todos});
-    console.log(todos.task);
+    Task.find({}, function(err, todos){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("todo", {todos: todos});
+        }
+    })
+    // res.render("todo", {todos: todos});
+    // console.log(todos.task);
 });
 /**
  * add to todo
