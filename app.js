7b6d0b4f79f9f9a4f1e59148616186d0505d03cd
efriedla <efriedla20852@gmail.com>
2018-02-05 14:57:38 -0800

    added mongoose with db name practice

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var request = require("request");
 var bodyparser = require("body-parser");
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/practice');
 
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static("public"));
